02a05deb3d78b23a5df3bff3da4f269e0c2103e1
Brian Moore <bjm8498@gmail.com>
2017-02-10 15:41:33 -0800

    This is checkpoint 11 up to date

---
+++ scripts/landing.js
@@ -0,0 +1,30 @@
+var points = document.getElementsByClassName('point');
+
+
+var revealPoint = function(point) {
+    point.style.opacity = 1;
+    point.style.transform = "scaleX(1) translateY(0)";
+    point.style.msTransform = "scaleX(1) translateY(0)";
+    point.style.WebkitTransform = "scaleX(1) translateY(0)";
+}
+
+var animatePoints = function(points) {
+ 
+    forEach(points, revealPoint);
+}; 
+
+window.onload = function(){
+    if(window.innerHeight > 950){
+        animatePoints(points);
+    }
+    
+    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+    
+    window.addEventListener('scroll', function(){
+        console.log("Current off set from the top is " + sellingPoints.getBoundingClientRect().top + "pixels");
+        if(document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance){
+            animatePoints(points);
+        }
+    })
+}
